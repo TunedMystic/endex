8df663a459928b92a23abd2f925a486a94a42477
Sandeep Jadoonanan <jsanweb@gmail.com>
2019-03-03 20:07:22 -0500

    Add fixtures for asset object creation. Work on simple models tests.

--- endex/tests/fixtures.py
+++ endex/tests/fixtures.py
@@ -1,6 +1,9 @@
 import mongoengine
 import pytest
 
+from endex.constants import ASSET_ETF, ASSET_STOCK
+from .factories import AssetFactory
+
 
 @pytest.fixture(scope='function')
 def mongo():
@@ -8,3 +11,18 @@ def mongo():
     yield db
     db.drop_database('testdb')
     db.close()
+
+
+@pytest.fixture
+def asset_obj():
+    return AssetFactory.build()
+
+
+@pytest.fixture
+def asset_etf():
+    return AssetFactory.build(type=ASSET_ETF)
+
+
+@pytest.fixture
+def asset_stock():
+    return AssetFactory.build(type=ASSET_STOCK)
--- endex/tests/test_models.py
+++ endex/tests/test_models.py
@@ -1,8 +1,17 @@
 import pytest
 
-from .fixtures import mongo
+
+from endex.models import Asset
+
+
+def test_asset_model_str(asset_stock):
+    a = asset_stock
+    assert str(a) == f'{a.symbol} | {a.type}'
+    assert str(a.address) == f'{a.address.street}, {a.address.city}, {a.address.country}'
+    assert str(a.contact) == f'{a.contact.email}, {a.contact.phone}'
 
 
 @pytest.mark.usefixtures('mongo')
-def test_something(mongo):
-    assert 1 == 1
+def test_asset_document_is_saved(asset_obj):
+    asset_obj.save()
+    assert Asset.objects.count() == 1
